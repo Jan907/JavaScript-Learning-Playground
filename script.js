d06071e7b3eef92737cbc568cee700b381b886cf
Jan <59793283+Jan907@users.noreply.github.com>
2025-02-07 19:16:45 +0100

    changed js

--- script.js
+++ script.js
@@ -1,37 +1,47 @@
 // Get access to the grid in which I want to display the books
-let grid = document.querySelector(".container");
+let grid = document.querySelector(".libgrid");
 let form = document.querySelector("form");
 
 // This is my library array that stores all the books
-const myLibrary = [];
+let myLibrary = [];
 
 // The constructor to create the books
-function book(name, author, topic, pages, status) {
-    this.name = name;
+function books(title, author, topic, pages, read, notread) {
+    this.title = title;
     this.author = author;
     this.topic = topic;
     this.pages = pages;
-    this.status = status; 
+    this.read = read;
+    this.notread = notread; 
 }
 
+//Create new book
+let book1 = new books("Hobbit", "Tolkien", "Fantasy", 200, "Read", "Notread");
+
+
 // Function adding Book to the Library
-function addBookToLibrary() {
+function addBookToLibrary(title, author, topic, pages, read, notread) {
     //Create new book
-    let newBook = new book(name, author, topic, pages, status);
+    let newBook = new books(title, author, topic, pages, read, notread);
     //Add new book to array
     myLibrary.push(newBook);
-    //Cycle through array and create visual dom representation
+};
+
+ //Cycle through array and create visual dom representation
+function displayBooks(title, author, topic, pages, read, notread) {
     for (let i = 0; i < myLibrary.length; i++) {
         let bookcard = document.createElement("div");
         bookcard.classList.add("card");
         bookcard.style.width = "100px";
         bookcard.style.height = "200px";
         bookcard.style.padding = "12px";
+        bookcard.style.borderRadius = "8px";
         bookcard.style.backgroundColor = "#8c8c8c";
-        bookcard.textContent = '${book1.name}, ${bookadd.author}, ${bookadd.topic}, ${bookadd.pages}, ${bookadd.status}';
+        bookcard.textContent = `${title}, ${author}, ${topic}, ${pages}, ${read}, ${notread}`;
         //Add book to the grid
-        document.grid.appendChild(bookcard);
+        grid.appendChild(bookcard);
     }
+    
 };
 
 
@@ -40,14 +50,15 @@ document.querySelector("form").addEventListener("submit", function(event) {
     event.preventDefault(); // Prevents the page from reloading
 
     //Get the data
-    let name = document.getElementById("name").value;
-    let author = document.getElementById("author").value;
-    let topic = document.getElementById("topic").value;
-    let pages = document.getElementById("pages").value;
-    let read = document.getElementById("read").value;
-    let notread = document.getElementById("notread").value;
+    let title = document.getElementById("name");
+    let author = document.getElementById("author");
+    let topic = document.getElementById("topic");
+    let pages = document.getElementById("pages");
+    let read = document.getElementById("read");
+    let notread = document.getElementById("notread");
 
     //Add book to the library
-    addBookToLibrary(name, author, topic, pages, read, notread);
-    
+    addBookToLibrary(title, author, topic, pages, read, notread);
+    //Visualize the books
+    displayBooks();
 });
